8faef51c327951893686c88c5299c931a7a23905
Katley Andrighetti <115327621+katleyc@users.noreply.github.com>
2023-12-10 17:16:02 -0300

    Com_thread2.java

--- Com_thread2.java
+++ Com_thread2.java
@@ -1,95 +1,76 @@
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
-public class Com_thread2 {
+class Com_thread1 {
 
-    private static final String[] NOUNS = {"puppy", "car", "rabbit", "girl", "monkey"};
-    private static final String[] VERBS = {"runs", "hits", "jumps", "drives", "barfs"};
-    private static final String[] ADJ = {"adorable", "clueless", "dirty", "odd", "stupid"};
-    private static final String[] ADV = {"crazily.", "dutifully.", "foolishly.", "merrily.", "occasionally."};
+    // Função para criar uma matriz aleatória
+    /**
+     * @param linhas
+     * @param colunas
+     * @return
+     */
+    public static double[][] inicializacao(int linhas, int colunas) {
+        final Random random = new Random();
+        double[][] matriz = new double[linhas][colunas];
 
-    public static void generateBigRandomSentences(String filename, int numeroLinhas, Object newParam) throws IOException {
-        Path path = Paths.get(filename);
-        Random random = new Random();
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                matriz[i][j] = random.nextDouble();
+            }
+        }
 
-        try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
-            for (int i = 0; i < numeroLinhas; i++) {
-                List<String> sentence = List.of(
-                        NOUNS[random.nextInt(NOUNS.length)],
-                        VERBS[random.nextInt(VERBS.length)],
-                        ADJ[random.nextInt(ADJ.length)],
-                        ADV[random.nextInt(ADV.length)]
-                );
-                writer.write(String.join(" ", sentence));
-                writer.newLine();
+        return matriz;
+    }
+
+    // Função para multiplicar duas matrizes
+    public static void multMatriz(double[][] matrizA, double[][] matrizB, int linhaInicio, int linhaFim, double[][] matrizR) {
+        for (int i = linhaInicio; i < linhaFim; i++) {
+            for (int j = 0; j < matrizB[0].length; j++) {
+                double sum = 0.0;
+                for (int k = 0; k < matrizA[0].length; k++) {
+                    sum += matrizA[i][k] * matrizB[k][j];
+                }
+                matrizR[i][j] = sum;
             }
         }
     }
-
-    public static void gerarArquivos(int numeroArquivos, int numeroLinhas) throws IOException {
-        for (int i = 0; i < numeroArquivos; i++) {
-            generateBigRandomSentences(String.format("arquivoTeste%02d.txt", i), numeroLinhas, null);
-        }
-    }
-
-    public static void replace(String substr, String newSubstr, int linhaInicio, int linhaFim) throws IOException {
-        for (int i = linhaInicio; i < linhaFim; i++) {
-            String filename = String.format("arquivoTeste%02d.txt", i);
-            Path filePath = Paths.get(filename);
-            String content = Files.readString(filePath, StandardCharsets.UTF_8);
-
-            // replace the substr by newSubstr
-            content = content.replace(substr, newSubstr);
-
-            // write data into the file
-            Files.writeString(filePath.resolveSibling(filename + ".alterado.txt"), content, StandardCharsets.UTF_8);
-        }
-    }
-
     public static void main(String[] args) {
-        int numeroArquivos = 500;
-        int numeroLinhas = 10_000;
-        int numThreads = 2;
-        int separacao = numeroArquivos / numThreads;
+        int linhasA = 1250;
+        int colunasA = 1000;
+        int linhasB = 1000;
+        int colunasB = 1250;
+
+        // Cria matrizes aleatórias
+        double[][] matrizA = inicializacao(linhasA, colunasA);
+        double[][] matrizB = inicializacao(linhasB, colunasB);
+
+        double[][] matrizR = new double[linhasA][colunasB]; // Matriz para armazenar o resultado
+
+        int numThreads = 5; // Número de threads
+
+        int separacao = linhasA / numThreads; // Cálculo de quanto cada thread terá da matriz
 
         for (int i = 0; i < 5; i++) {
             long startTime = System.currentTimeMillis();
+            Thread[] threads = new Thread[numThreads];
+            for (int j = 0; j < numThreads; j++) {
+                int linhaInicio = j * separacao;
+                int linhaFim = (j + 1) * separacao < linhasA ? (j + 1) * separacao : linhasA;
+
+                threads[j] = new Thread(() -> multMatriz(matrizA, matrizB, linhaInicio, linhaFim, matrizR));
+                threads[j].start();
+            }
 
             try {
-                gerarArquivos(numeroArquivos, numeroLinhas);
-
-                ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
-                for (int j = 0; j < numThreads; j++) {
-                    int linhaInicio = j * separacao;
-                    int linhaFim = (j + 1) * separacao < numeroArquivos ? (j + 1) * separacao : numeroArquivos;
-
-                    executorService.execute(() -> {
-                        try {
-                            replace("car", "truck", linhaInicio, linhaFim);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    });
+                for (Thread thread : threads) {
+                    thread.join(); // Sincroniza as threads
                 }
-
-                executorService.shutdown();
-                executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-
-            } catch (IOException | InterruptedException e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
             long endTime = System.currentTimeMillis();
-            System.out.printf("Levou %.4f segundos para processar.%n", (endTime - startTime) / 1000.0);
+
+            System.out.printf("Tempo de execução com %d threads: %.4f segundos%n", numThreads, (endTime - startTime) / 1000.0);
         }
     }
 }
